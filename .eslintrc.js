46ac5c02e3978497f0cabb62dacebd1c2169bc3f
Anton <office.sklyarov@gmail.com>
2020-11-19 11:25:27 +0200

    eslint was added

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    '@mate-academy/eslint-config',
+    "plugin:react/recommended"
+  ],
+  rules: {
+    strict: 'off',
+  },
+  parserOptions: {
+    "sourceType": "module",
+  }
+};
